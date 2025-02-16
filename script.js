fd299eb674da6d43430e66700250194f235c6fde
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-16 18:34:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ class Car {
 		this.model=model;
 	}
 	getMakeModel(){
-		return this.make+ this.model
+		return `${this.make+this.model}`
 	}
 }
 
 class SportsCar extends Car {
 	constructor(make,model,topSpeed) {
-		super(make,model,topSpeed);
+		super(make,model);
 		this.topSpeed=topSpeed
 		
 	}
