b8de845dbfb8794309efa19f3040843ca7813c20
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-16 18:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-// Complete the js code
-function Car(make, model) {}
+// Complete the js codr
+class Car {
+	constructor(make,model) {
+		this.make=make;
+		this.model=model;
+	}
+	getMakeModel(){
+		return this.make+ this.model
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car {
+	constructor(make,model,topSpeed) {
+		super(make,model,topSpeed);
+		this.topSpeed=topSpeed
+		
+	}
+
+	getTopSpeed(){
+		return this.topSpeed
+	}
+}
+// function Car(make, model) {
+// 	this.make=make;
+// 	this.model=model
+// }
+
+// function SportsCar(make, model, topSpeed) {
+// 	super(make,model)
+	
+// }
 
 // Do not change the code below
 window.Car = Car;
